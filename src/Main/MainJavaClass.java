69dfea135bab3dd5974897b6c468499aadad3279
Lotadyy <me@carterhendricks.com>
2022-03-19 18:29:59 -0700

    fix example.ExampleMod error (2)

--- src/Main/MainJavaClass.java
+++ src/Main/MainJavaClass.java
@@ -9,9 +9,9 @@ import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class MainJavaClass extends Mod{
 
-    public ExampleJavaMod(){
+    public MainJavaClass(){
         Log.info("Loaded ExampleJavaMod constructor.");
 
         //listen for game load event
